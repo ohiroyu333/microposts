a45bf9824ec1cf18a8b419d4884a9d5ad3fa9320
ohiroyu333 <o_hiroyu333@yahoo.co.jp>
2016-03-31 03:05:05 +0000

    add profile update2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
 
   def show
    @user = User.find(params[:id])
@@ -39,4 +40,11 @@ class UsersController < ApplicationController
                                  :password_confirmation,
                                  :location, :URL, :introduction)
   end
+  
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end    
 end
