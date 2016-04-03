ec4b4bb0d5a8ecb4ee8e7ca56b33ffd94394c74d
ohiroyu333 <o_hiroyu333@yahoo.co.jp>
2016-04-03 13:30:31 +0000

    add followings-followers list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:edit, :update,
+                                       :following, :followers]
 
   def show
    @user = User.find(params[:id])
@@ -32,6 +33,18 @@ class UsersController < ApplicationController
     else
       render 'edit'
     end
+  end
+  
+  def following
+    @user  = User.find(params[:id])
+    @users = @user.following_users
+    render 'show_follow'
+  end
+
+  def followers
+    @user  = User.find(params[:id])
+    @users = @user.follower_users
+    render 'show_follower'
   end
 
   private
